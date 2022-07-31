e00be76d0c58de1addba039e43063a03fd094dc0
Alfredo Morieri <fred.morieri@gmail.com>
2022-07-31 21:48:11 +0000

    Fix db url

--- codestar/settings.py
+++ codestar/settings.py
@@ -85,19 +85,19 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if development:
-    print("loading sqlite db ...")
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    print("loading -postgres db....")
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-    }
+# if development:
+#     print("loading sqlite db ...")
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
+# else:
+print("loading -postgres db....")
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 
 # Password validation
