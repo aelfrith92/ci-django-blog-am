483bf02c1262c159eaceb4287ca2a293eca94756
Alfredo Morieri <fred.morieri@gmail.com>
2022-08-02 18:12:04 +0000

    Deploy attempt

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = development
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'codestar-am-django.herokuapp.com',
@@ -96,7 +96,7 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # else:
 print("loading -postgres db....")
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
